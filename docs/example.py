e2ebb37706284355f40e71b0c8daa70ae6d66b59
joshua.caldwell <jkcaldwe@gmail.com>
2019-05-27 08:10:11 -0400

    Implemented dictionaries for storing data in list. Other improvements

--- docs/example.py
+++ docs/example.py
@@ -1,6 +1,7 @@
 from Robinhood import Robinhood
 # Threading allows for timers to be run
 import threading
+import time
 # need to get JSON data given a URL and process the JSON data
 from urllib.request import urlopen
 import json
@@ -15,35 +16,22 @@ def main():
 	# structure to keep each stock data in
 	stock_data = namedtuple("stock_data", "field1 field2 field3");
 
-	#Pull Robinhood login data from text file so not exposed in code
-	lines = [] #Declare an empty list named "lines"
-	with open ('C:\\Users\\joshua.caldwell\\Stock_App\\RH_auth.txt', 'rt') as in_file:  #Open file lorem.txt for reading of text data.
-		for line in in_file:
-			lines.append(line);  #add that line to our list of lines.
-	username = lines[0].rstrip();
-	password = lines[1].rstrip();
-	qr_code = lines[2];
-	# print(username + ":"+ password + ":" + qr_code);        #print the list object.
-
-	#Setup and open my trader
-	my_trader = Robinhood();
-	#login to Robinhood using username and password from file
-	my_trader.login(username=username, password=password, qr_code=qr_code)
+	#log in and return trade handle
+	my_trader = login();
 
 	# print(my_trader.positions());
 	position_symbols = getCurrentPositions(my_trader);
 	print(position_symbols);
 
-	# create arrays for each position symbol to store values
-	position_quotes = defaultdict(list);
-	for symbol in position_symbols:
-		position_quotes[symbol].append(1) ;
-	
-	position_quotes[position_symbols[0]].append(7);
-	print (position_quotes[position_symbols[0]]);
-		
-	#Get stock price
-	print (getQuoteBySymbol("aapl"));
+	#Create data array for each symbol in my positions and initialize with current quote
+	position_quotes_init = defaultdict(list);
+	position_quotes = populateQuoteData(position_symbols, position_quotes_init);
+
+	#Call function to add quote data to position quotes
+	run = True;
+	# while (run):
+	# 	position_quotes = populateQuoteData(position_symbols, position_quotes);
+	# 	time.sleep(30);
 
 	#Get stock information
 	    #Note: Sometimes more than one instrument may be returned for a given stock symbol
@@ -73,7 +61,7 @@ def main():
 
 	# #Place a sell order
 	# sell_order = my_trader.place_sell_order(stock_instrument, 1)
-
+#----------------------------------------------------------------------------------------------------------------------------
 def getCurrentPositions(my_trader):
 	#get current bought in postiions
 	#Gets the total number of open positions also past positions since many are 0.  This filters based only on positions where there are a quantity of stocks
@@ -87,13 +75,38 @@ def getCurrentPositions(my_trader):
 			# print ("symbol: " + json_data['symbol'] + " quantity: " + my_trader.positions()['results'][i]['quantity']);
 			
 	return symbols;
-
+#----------------------------------------------------------------------------------------------------------------------------
+def populateQuoteData(position_symbols, position_quotes):
+	# create arrays for each position symbol to store values
+	for symbol in position_symbols:
+		position_quotes[symbol].append(getQuoteBySymbol(symbol));
+	
+	print (position_quotes);
+	return position_quotes;
+#----------------------------------------------------------------------------------------------------------------------------
 def getQuoteBySymbol(symbol):
-	return (si.get_live_price("aapl"));
+	return (si.get_live_price(symbol));
+#----------------------------------------------------------------------------------------------------------------------------
+def login():
+	#Pull Robinhood login data from text file so not exposed in code
+	lines = [] #Declare an empty list named "lines"
+	with open ('C:\\Users\\joshua.caldwell\\Stock_App\\RH_auth.txt', 'rt') as in_file:  #Open file lorem.txt for reading of text data.
+		for line in in_file:
+			lines.append(line);  #add that line to our list of lines.
+	username = lines[0].rstrip();
+	password = lines[1].rstrip();
+	qr_code = lines[2];
+	# print(username + ":"+ password + ":" + qr_code);        #print the list object.
 
-
+	#Setup and open my trader
+	my_trader = Robinhood();
+	#login to Robinhood using username and password from file
+	my_trader.login(username=username, password=password, qr_code=qr_code);
+	return my_trader;
+#----------------------------------------------------------------------------------------------------------------------------
 def exitApp():
+	print ("exiting");
 	exit();
-
+#----------------------------------------------------------------------------------------------------------------------------
 # Call the main function.  Did this way so I could define functions of main later in code.
 main();
