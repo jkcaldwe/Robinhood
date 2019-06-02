a37eb3b8df2d02e64c8bc8d450c0d6d1a825a5d4
joshua.caldwell <jkcaldwe@gmail.com>
2019-06-02 12:42:47 -0400

    refactored so main processing is done through qtapp and auto_trader has support functions.  Timer working and variables are defined globally in class.

--- docs/auto_trader.py
+++ docs/auto_trader.py
@@ -20,89 +20,79 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtCore
 from PyQt5.QtCore import QObject
 
-class TraderApp(QObject):
+# class TraderApp(QObject):
     # Global position quotes to be used by timer
-    position_quotes = [];
-    #----------------------------------------------------------------------------------------------------------------------------
-    def main(self):
-        logging.basicConfig(level=logging.DEBUG)
-        # structure to keep each stock data in
-        # stock_data = namedtuple("stock_data", "field1 field2 field3");
+position_quotes = [];
+#----------------------------------------------------------------------------------------------------------------------------
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    # structure to keep each stock data in
+    # stock_data = namedtuple("stock_data", "field1 field2 field3");
 
-        #log in and return trade handle
-        my_trader = self.login();
+    #log in and return trade handle
+    my_trader = login();
 
-        # print(my_trader.positions());
-        position_symbols = self.getCurrentPositions(my_trader);
-        logging.info(position_symbols);
+    # print(my_trader.positions());
+    position_symbols = getCurrentPositions(my_trader);
+    logging.info(position_symbols);
 
-        #Create data array for each symbol in my positions and initialize with current quote
-        position_quotes_init = defaultdict(list);
-        position_quotes = self.populateQuoteData(position_symbols, position_quotes_init);
+    #Create data array for each symbol in my positions and initialize with current quote
+    position_quotes_init = defaultdict(list);
+    position_quotes = populateQuoteData(position_symbols, position_quotes_init);
 
-        #Call function to add quote data to position quotes
-        logging.info ("Before Timer");
-        
-        timer.timeout.connect(self.updateQuoteData)
-        timer.start(500)
-        # while (run):
-        #   position_quotes = populateQuoteData(position_symbols, position_quotes);
-        #   time.sleep(30);
+    # while (run):
+    #   position_quotes = populateQuoteData(position_symbols, position_quotes);
+    #   time.sleep(30);
 
-    #----------------------------------------------------------------------------------------------------------------------------
-    def getCurrentPositions(self, my_trader):
-        #get current bought in postiions
-        #Gets the total number of open positions also past positions since many are 0.  This filters based only on positions where there are a quantity of stocks
-        symbols = [];
-        for i in range (len(my_trader.positions()['results'])):
-            if (float(my_trader.positions()['results'][i]['quantity']) != 0):
-                #Index one of the open positions and get the instrument URL then find the symbol for the position
-                response = urlopen(my_trader.positions()['results'][i]['instrument']);
-                json_data = json.loads(response.read());
-                symbols.append(json_data['symbol']);
-        # print ("symbol: " + json_data['symbol'] + " quantity: " + my_trader.positions()['results'][i]['quantity']);
+#----------------------------------------------------------------------------------------------------------------------------
+def getCurrentPositions(my_trader):
+    #get current bought in postiions
+    #Gets the total number of open positions also past positions since many are 0.  This filters based only on positions where there are a quantity of stocks
+    symbols = [];
+    for i in range (len(my_trader.positions()['results'])):
+        if (float(my_trader.positions()['results'][i]['quantity']) != 0):
+            #Index one of the open positions and get the instrument URL then find the symbol for the position
+            response = urlopen(my_trader.positions()['results'][i]['instrument']);
+            json_data = json.loads(response.read());
+            symbols.append(json_data['symbol']);
+    # print ("symbol: " + json_data['symbol'] + " quantity: " + my_trader.positions()['results'][i]['quantity']);
 
-        return symbols;
-    #----------------------------------------------------------------------------------------------------------------------------
-    def populateQuoteData(self, position_symbols, position_quotes):
-        # create arrays for each position symbol to store values
-        for symbol in position_symbols:
-            position_quotes[symbol].append(self.getQuoteBySymbol(symbol));
+    return symbols;
+#----------------------------------------------------------------------------------------------------------------------------
+def populateQuoteData(position_symbols, position_quotes):
+    # create arrays for each position symbol to store values
+    for symbol in position_symbols:
+        position_quotes[symbol].append(getQuoteBySymbol(symbol));
 
-        logging.info (position_quotes);
-        return position_quotes;
-    #----------------------------------------------------------------------------------------------------------------------------
-    def getQuoteBySymbol(self, symbol):
-        return (si.get_live_price(symbol));
-    #----------------------------------------------------------------------------------------------------------------------------
-    def login(self):
-        #Pull Robinhood login data from text file so not exposed in code
-        lines = [] #Declare an empty list named "lines"
-        with open ('C:\\Users\\joshua.caldwell\\Stock_App\\RH_auth.txt', 'rt') as in_file:  #Open file lorem.txt for reading of text data.
-            for line in in_file:
-                lines.append(line);  #add that line to our list of lines.
-        username = lines[0].rstrip();
-        password = lines[1].rstrip();
-        qr_code = lines[2];
-        # print(username + ":"+ password + ":" + qr_code);        #print the list object.
+    logging.info (position_quotes);
+    return position_quotes;
+#----------------------------------------------------------------------------------------------------------------------------
+def getQuoteBySymbol(symbol):
+    return (si.get_live_price(symbol));
+#----------------------------------------------------------------------------------------------------------------------------
+def login():
+    #Pull Robinhood login data from text file so not exposed in code
+    lines = [] #Declare an empty list named "lines"
+    with open ('C:\\Users\\joshua.caldwell\\Stock_App\\RH_auth.txt', 'rt') as in_file:  #Open file lorem.txt for reading of text data.
+        for line in in_file:
+            lines.append(line);  #add that line to our list of lines.
+    username = lines[0].rstrip();
+    password = lines[1].rstrip();
+    qr_code = lines[2];
+    # print(username + ":"+ password + ":" + qr_code);        #print the list object.
 
-        #Setup and open my trader
-        my_trader = Robinhood();
-        #login to Robinhood using username and password from file
-        my_trader.login(username=username, password=password, qr_code=qr_code);
-        return my_trader;
-    #----------------------------------------------------------------------------------------------------------------------------
-    def updateQuoteData(self):
-        print ('in');
-        logging.info ("timer");
-    #----------------------------------------------------------------------------------------------------------------------------
-    def exitApp(self):
-        logging.info ("exiting");
-        exit();
+    #Setup and open my trader
+    my_trader = Robinhood();
+    #login to Robinhood using username and password from file
+    my_trader.login(username=username, password=password, qr_code=qr_code);
+    return my_trader;
+
+def exitApp():
+    logging.info ("exiting");
+    exit();
 #----------------------------------------------------------------------------------------------------------------------------
 
 #configure logging to print debug messages on the screen
-timer = QtCore.QTimer()
 #Start the widget and runs main code.  Eventually hook data into widget through slots
 #if __name__ == '__main__':   
 # app = QApplication(sys.argv)
--- docs/qtapp.py
+++ docs/qtapp.py
@@ -3,6 +3,8 @@ from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
 from PyQt5 import QtCore
 import logging
 import auto_trader
+#Support default dict list
+from collections import defaultdict
 
 #configure logging to print debug messages on the screen
 logging.basicConfig(level=logging.DEBUG)
@@ -14,8 +16,14 @@ class TraderGui(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+
+        # Define global variables for the class instance
+        self.timer = QtCore.QTimer();
+        self.position_quotes = [];
+        self.position_symbols = [];
         # Call main trader logic
-        tradeLogic = auto_trader.TraderApp().main();
+        # tradeLogic = auto_trader.TraderApp().main();
+        self.main();
         
     def initUI(self):      
 
@@ -33,21 +41,38 @@ class TraderGui(QMainWindow):
         self.setGeometry(300, 300, 290, 150)
         self.setWindowTitle('Event sender')
         self.show()
-
-        timer.timeout.connect(self.timerEvent);
-        timer.start(500);
-        
     
+    def main(self):
+        #log in and return trade handle.  'self' defines it as a global across the class
+        self.my_trader = auto_trader.login();
+
+        # print(my_trader.positions());
+        self.position_symbols = auto_trader.getCurrentPositions(self.my_trader);
+        logging.info(self.position_symbols);
+
+        #Create data array for each symbol in my positions and initialize with current quote
+        position_quotes_init = defaultdict(list);
+        self.position_quotes = auto_trader.populateQuoteData(self.position_symbols, position_quotes_init);
+        
+        self.timer.timeout.connect(self.timerEvent);
+        self.timer.start(60000);
+
     def buttonClicked(self):
         sender = self.sender()
         self.statusBar().showMessage(sender.text() + ' was pressed')
         logging.info(sender.text() + ' was pressed');
 
     def timerEvent(self):
-        logging.info("Timer emited");    
+        logging.info("Timer emited");
+        self.position_symbols = auto_trader.getCurrentPositions(self.my_trader);
+        logging.info(self.position_symbols);    
 
 
-timer = QtCore.QTimer();
+#Define timer as global so it will keep running while app is alive
+# timer = QtCore.QTimer();
+# Global to get position quotes
+# position_quotes = [];
+#Start application
 app = QApplication(sys.argv)
 autoTrader = TraderGui()
 sys.exit(app.exec_())
